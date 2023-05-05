668a5c57cbdf80a6330d6efb4ed6529b30ce43cb
vik0t0r <victordpa7@gmail.com>
2023-05-05 13:04:28 +0200

    falta incremental 1

--- backup.py
+++ backup.py
@@ -15,12 +15,31 @@ def make_tarfile(output_filename, s_dir):
     with tarfile.open(output_filename, "w:gz") as tar:
         tar.add(s_dir, arcname=os.path.basename(s_dir))
 
+def make_tarfile_incremental(output_filename, s_dir, reference_filename):
+    with tarfile.open(output_filename, "w:gz") as tar:
+
+
 
 # coje dos directorios, y hace una backup del primero en el segundo
 def crear_backup_completa(source, destination):
     nombre_backup = "{}.tar.gpg".format(int(time.time()))
     path_tarfile = tmp_dir+"/" + "out.tar.gz"
     make_tarfile(path_tarfile, source)
+    path_encriptado = path_tarfile+".gpg"
+    x = subprocess.run(["gpg","--batch","--no-symkey-cache","--passphrase",password,"-c","-o",path_encriptado, path_tarfile])
+    os.rename(path_encriptado, destination + "/" + nombre_backup)
+
+def crear_backup_incremental(source, destination, refs):
+    if os.listdir(refs) == 0:
+        crear_backup_completa(source,refs)
+
+    # archivo de backup semanal
+    nombre_archivo_referencia = max(os.listdir(refs))
+    archivo_referencia = refs + "/" + nombre_archivo_referencia
+
+    nombre_backup = "{}_{}.tar.gpg".format(int(time.time()), nombre_archivo_referencia)
+    path_tarfile = tmp_dir + "/" + "out.tar.gz"
+    make_tarfile_incremental(path_tarfile, source, archivo_referencia)
     path_encriptado = path_tarfile+".gpg"
     x = subprocess.run(["gpg","--batch","--no-symkey-cache","--passphrase",password,"-c","-o",path_encriptado, path_tarfile])
     os.rename(path_encriptado, destination + "/" + nombre_backup)
@@ -54,17 +73,26 @@ with open("/proc/mounts", "r") as f:
         particion, punto_montaje, tipo_fs, opciones, _, _ = linea.split()
         montajes[punto_montaje] = particion
 
-ocupacion = shutil.disk_usage(montajes[backup_dir])
+ocupacion_used = check_espacio_disponible(backup_dir)
+if ocupacion_used >= max_size*0.85:
+     print("Aviso para el administrador, queda poco espacio para guardar las copias de seguridad.\n"
+           "No se va a realizar ninguna copia de seguridad hasta que el problema esté solucionado.")
+     exit(0)
 
-if ocupacion.total >= max_size*0.85:
-    print("Aviso para el administrador, queda poco espacio para guardar las copias de seguridad.\n"
-          "No se va a realizar ninguna copia de seguridad hasta que el problema esté solucionado.")
-    exit(1)
+# checkear estructura del backupdir
 
-#Programa principal
+check_structura_backup(backup_dir)
 
 if tipo_copia == "-d":
-     pass
+    lista_archivos_diaria = os.listdir(backup_dir + "/diaria")
+    n_backups_diaria = len(lista_archivos_diaria)
+    backup_mas_vieja_diaria = min(lista_archivos_diaria)
+    if n_backups_diaria >= 7:
+        os.remove(backup_mas_vieja_diaria)
+
+    crear_backup_incremental(source_dir, backup_dir + "/diaria", backup_dir + "/semanal")
+    exit(0)
+
 
 if tipo_copia == "-w":
     lista_archivos = os.listdir(backup_dir + "/semanal")
@@ -74,6 +102,7 @@ if tipo_copia == "-w":
         os.remove(backup_mas_vieja)
 
     crear_backup_completa(source_dir, backup_dir + "/semanal")
+    exit(0)
 
 
 if tipo_copia == "-m":
@@ -82,7 +111,7 @@ if tipo_copia == "-m":
     backup_mas_vieja = min(lista_archivos)
 
     crear_backup_completa(source_dir, backup_dir + "/mensual")
-    exit(1)
+    exit(0)
 
 print("Argumento mal utilizado, modo de uso: Primer parámetro(-w, -d, -m)")
 exit(1)
