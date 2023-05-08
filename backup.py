0e72ad7ac5e5b089f33dd5e39ce1202a95912ad0
vicdeaj <al404303@uji.es>
2023-05-08 20:14:03 +0200

    implementacion incremental 1

--- backup.py
+++ backup.py
@@ -1,6 +1,6 @@
-#from Crypto.Cipher import AES
-#from Crypto.Protocol.KDF import PBKDF2
-#from Crypto.Random import get_random_bytes
+# from Crypto.Cipher import AES
+# from Crypto.Protocol.KDF import PBKDF2
+# from Crypto.Random import get_random_bytes
 import os, sys
 import configparser
 import shutil
@@ -8,6 +8,8 @@ import tarfile
 import uuid
 import subprocess
 import time
+import glob
+
 
 # backup semanal
 
@@ -15,25 +17,43 @@ def make_tarfile(output_filename, s_dir):
     with tarfile.open(output_filename, "w:gz") as tar:
         tar.add(s_dir, arcname=os.path.basename(s_dir))
 
+
 def make_tarfile_incremental(output_filename, s_dir, reference_filename):
+    archivos_vistos = set()
+    timestamp_ref_backup = int(reference_filename.split("/")[4].split(".")[0])
+    print(timestamp_ref_backup)  # timestamp de la backup de referencia
     with tarfile.open(output_filename, "w:gz") as tar:
         for file in glob.glob(os.path.join(s_dir, '**'), recursive=True):
-            print(file)
+            if os.path.isfile(file):
+                archivos_vistos.add(file) # luego lo usaremos para ver que archivos han sido eliminados
+                if int(os.path.getmtime(file)) > timestamp_ref_backup:
+                    tar.add(file, arcname=os.path.relpath(file,s_dir))  # fecha modificacion del archivo
+
+    # decipher de tarfile
+    x = subprocess.run(
+        ["gpg", "--batch", "--no-symkey-cache", "--passphrase", password, "-c", "-o", path_to_encrypted, path_tarfile]) # esto hay que arreglarlo para descifrar
+    # mirar si cada archivo esta en archivos_vistos
+    with tarfile.open(reference_filename, "r:gz"):
+
+    # si no esta en archivos_vistos lo añadimos a un archivo ccon la lista de archivos eliminados
+    # añadimos ese archivo al
 
 
 
 # coje dos directorios, y hace una backup del primero en el segundo
 def crear_backup_completa(source, destination):
     nombre_backup = "{}.tar.gpg".format(int(time.time()))
-    path_tarfile = tmp_dir+"/" + "out.tar.gz"
+    path_tarfile = tmp_dir + "/" + "out.tar.gz"
     make_tarfile(path_tarfile, source)
-    path_encriptado = path_tarfile+".gpg"
-    x = subprocess.run(["gpg","--batch","--no-symkey-cache","--passphrase",password,"-c","-o",path_encriptado, path_tarfile])
+    path_encriptado = path_tarfile + ".gpg"
+    x = subprocess.run(
+        ["gpg", "--batch", "--no-symkey-cache", "--passphrase", password, "-c", "-o", path_encriptado, path_tarfile])
     os.rename(path_encriptado, destination + "/" + nombre_backup)
+
 
 def crear_backup_incremental(source, destination, refs):
     if os.listdir(refs) == 0:
-        crear_backup_completa(source,refs)
+        crear_backup_completa(source, refs)
 
     # archivo de backup semanal
     nombre_archivo_referencia = max(os.listdir(refs))
@@ -42,9 +62,11 @@ def crear_backup_incremental(source, destination, refs):
     nombre_backup = "{}_{}.tar.gpg".format(int(time.time()), nombre_archivo_referencia)
     path_tarfile = tmp_dir + "/" + "out.tar.gz"
     make_tarfile_incremental(path_tarfile, source, archivo_referencia)
-    path_encriptado = path_tarfile+".gpg"
-    x = subprocess.run(["gpg","--batch","--no-symkey-cache","--passphrase",password,"-c","-o",path_encriptado, path_tarfile])
+    path_encriptado = path_tarfile + ".gpg"
+    x = subprocess.run(
+        ["gpg", "--batch", "--no-symkey-cache", "--passphrase", password, "-c", "-o", path_encriptado, path_tarfile])
     os.rename(path_encriptado, destination + "/" + nombre_backup)
+
 
 def check_espacio_disponible(backup_dir):
     path_semanal = backup_dir + "/semanal"
@@ -73,7 +95,7 @@ def check_structura_backup(path):
         os.mkdir(backup_dir + "/diaria")
 
 
-#configparser
+# configparser
 configfile = "~/.backupconfig"
 config = configparser.ConfigParser()
 config.read(os.path.expanduser(configfile))
@@ -83,10 +105,9 @@ source_dir = config.get("BasicConfig", "source_dir")
 password = config.get("BasicConfig", "password")
 
 tmp_dir = "/tmp/" + str(uuid.uuid4())
-os.mkdir(tmp_dir) #Guardar el archivo comprimido antes de encriptarlo
+os.mkdir(tmp_dir)  # Guardar el archivo comprimido antes de encriptarlo
 
-
-#Programa principal
+# Programa principal
 
 # Recogida y comprobación de los argumentos
 tipo_copia = sys.argv[1]
@@ -97,13 +118,13 @@ if num_args != 1:
 
 # establecer una ocupación máxima de disco (avisar antes de superarlo)
 
-max_size = 100000000000 #100gb
+max_size = 100000000000  # 100gb
 
 ocupacion_used = check_espacio_disponible(backup_dir)
-if ocupacion_used >= max_size*0.85:
-     print("Aviso para el administrador, queda poco espacio para guardar las copias de seguridad.\n"
-           "No se va a realizar ninguna copia de seguridad hasta que el problema esté solucionado.")
-     exit(0)
+if ocupacion_used >= max_size * 0.85:
+    print("Aviso para el administrador, queda poco espacio para guardar las copias de seguridad.\n"
+          "No se va a realizar ninguna copia de seguridad hasta que el problema esté solucionado.")
+    exit(0)
 
 # checkear estructura del backupdir
 
@@ -116,11 +137,10 @@ if tipo_copia == "-d":
     if n_backups_diaria > 0:
         backup_mas_vieja_diaria = min(lista_archivos_diaria)
         if n_backups_diaria >= 7:
-            os.remove(backup_mas_vieja_diaria)
+            os.remove(backup_dir + "/diaria/" + backup_mas_vieja_diaria)
 
     crear_backup_incremental(source_dir, backup_dir + "/diaria", backup_dir + "/semanal")
     exit(0)
-
 
 if tipo_copia == "-w":
     lista_archivos_semanal = os.listdir(backup_dir + "/semanal")
@@ -129,28 +149,24 @@ if tipo_copia == "-w":
     if n_backups_semanales > 0:
         backup_mas_vieja_semanal = min(lista_archivos_semanal)
         if n_backups_semanales >= 4:
-            os.remove(backup_mas_vieja_semanal)
+            os.remove(backup_dir + "/semanal/" + backup_mas_vieja_semanal)
 
     crear_backup_completa(source_dir, backup_dir + "/semanal")
     exit(0)
 
-
 if tipo_copia == "-m":
-    lista_archivos_mensual= os.listdir(backup_dir + "/mensual")
+    lista_archivos_mensual = os.listdir(backup_dir + "/mensual")
     n_backups_mensual = len(lista_archivos_mensual)
 
     if n_backups_mensual > 0:
         backup_mas_vieja_mensual = min(lista_archivos_mensual)
         if n_backups_mensual >= 12:
-            os.remove(backup_mas_vieja_mensual)
+            os.remove(backup_dir + "/mensual/" + backup_mas_vieja_mensual)
 
     crear_backup_completa(source_dir, backup_dir + "/mensual")
     exit(0)
 
 print("Argumento mal utilizado, modo de uso: Primer parámetro(-w, -d, -m)")
 exit(1)
-
-
-
 
 shutil.rmtree(tmp_dir)
